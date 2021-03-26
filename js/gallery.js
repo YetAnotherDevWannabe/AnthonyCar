d0476fffa5832c22f987c428d5c3c2923bd80d79
YetAnotherDevWannabe <oferriot.github@gmail.com>
2021-03-26 15:01:48 +0100

    gallery.js 100% done

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,41 @@
-$('#formSubmit').click(function (e)
-{
-	e.preventDefault();	// Prevent form from refreshing page
+// Function to display image in overlay
+function displayImageOverlay(imageClicked) {
+
+	// Create overlay html variable
+	let overlayDiv = $(`
+	<div class="overlay">
+		<div class="close">X</div>
+		<img src="img/` + imageClicked + `" alt="` + imageClicked + `">
+	</div>`)
+
+	// Add html to page
+	$('body').prepend(overlayDiv);
+
+
+	$('.overlay .close').click(function () {
+		removeImageOverlay();
+	})
+
+}
+
+// Function to remove image overlay
+function removeImageOverlay() {
+	$('.overlay').remove();
+}
 
 
 
 
+// faseIn presentation text when page loads
+$.when( $.ready ).then(function() {
+	$('#hiddenText').fadeIn(500);
+});
+
+
+// Click event listener
+$('#carList div figure img').click(function () {
+
+	$('#hiddenText').stop();
+
+	displayImageOverlay( $(this).data('image') );
 })
